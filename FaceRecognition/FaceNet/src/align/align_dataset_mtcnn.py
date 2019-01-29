66e97767ab18d33ae133fb0a135b0fb242d19eba
Fantasy <fantasyjxf@qq.com>
2019-01-29 18:13:41 +0800

    The FaceNet mtcnn acts poor

--- FaceRecognition/FaceNet/src/align/align_dataset_mtcnn.py
+++ FaceRecognition/FaceNet/src/align/align_dataset_mtcnn.py
@@ -28,15 +28,58 @@ from __future__ import print_function
 from scipy import misc
 import sys
 import os
-import argparse
 import tensorflow as tf
 import numpy as np
-import facenet
-import align.detect_face
+from FaceRecognition.FaceNet.src import facenet,align
+from FaceRecognition.FaceNet.src.align import detect_face
 import random
 from time import sleep
+import argparse
 
-def main(args):
+"""parsing and configuration"""
+def parse_args():
+    desc = "Tensorflow implementation of Face Alignment"
+    parser = argparse.ArgumentParser(description=desc)
+
+    parser.add_argument('--input_dir', type=str, help='Directory with unaligned images.', default='../../data/')
+    parser.add_argument('--output_dir', type=str, help='Directory with aligned face thumbnails.', default='./result')
+    parser.add_argument('--image_size', type=int,
+        help='Image size (height, width) in pixels.', default=182)
+    parser.add_argument('--margin', type=int,
+        help='Margin for the crop around the bounding box (height, width) in pixels.', default=44)
+    parser.add_argument('--random_order', 
+        help='Shuffles the order of images to enable alignment using multiple processes.', action='store_true')
+    parser.add_argument('--gpu_memory_fraction', type=float,
+        help='Upper bound on the amount of GPU memory that will be used by the process.', default=1.0)
+    parser.add_argument('--detect_multiple_faces', type=bool,
+                        help='Detect and align multiple faces per image.', default=False)
+
+    return check_args(parser.parse_args())
+
+"""checking arguments"""
+def check_args(args):
+    try:
+        assert os.path.exists(args.input_dir)
+    except:
+        print('There is no such path%s'%args.input_dir)
+        return None
+
+    try:
+        assert os.path.exists(args.output_dir)
+    except:
+        print('There is no such path%s'%args.output_dir)
+        return None
+
+    return args
+
+"""main"""
+def main():
+
+    # parse arguments
+    args = parse_args()
+    if args is None:
+        exit()
+
     sleep(random.random())
     output_dir = os.path.expanduser(args.output_dir)
     if not os.path.exists(output_dir):
@@ -137,23 +180,5 @@ def main(args):
     print('Total number of images: %d' % nrof_images_total)
     print('Number of successfully aligned images: %d' % nrof_successfully_aligned)
             
-
-def parse_arguments(argv):
-    parser = argparse.ArgumentParser()
-    
-    parser.add_argument('input_dir', type=str, help='Directory with unaligned images.')
-    parser.add_argument('output_dir', type=str, help='Directory with aligned face thumbnails.')
-    parser.add_argument('--image_size', type=int,
-        help='Image size (height, width) in pixels.', default=182)
-    parser.add_argument('--margin', type=int,
-        help='Margin for the crop around the bounding box (height, width) in pixels.', default=44)
-    parser.add_argument('--random_order', 
-        help='Shuffles the order of images to enable alignment using multiple processes.', action='store_true')
-    parser.add_argument('--gpu_memory_fraction', type=float,
-        help='Upper bound on the amount of GPU memory that will be used by the process.', default=1.0)
-    parser.add_argument('--detect_multiple_faces', type=bool,
-                        help='Detect and align multiple faces per image.', default=False)
-    return parser.parse_args(argv)
-
 if __name__ == '__main__':
-    main(parse_arguments(sys.argv[1:]))
+    main()
